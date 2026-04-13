52c76c1a45ff8d680d2f04f031d9bcb4f9f4f392
Akshay Kumar <taara0361@gmai.com>
2026-04-13 22:41:13 +0530

    learn positioning in tailwind

---
+++ dashboardprototype/src/App.tsx
@@ -0,0 +1,15 @@
+import Sidebar from "./components/sidebar"
+
+
+
+function App() {
+
+  return (
+    <div className="flex flex-1">
+    <Sidebar/>
+    <h1>Begin building a dashboard</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ dashboardprototype/src/components/sidebar.tsx
@@ -0,0 +1,10 @@
+const Sidebar=()=>{
+    return(
+    <div className="w-55 h-262 flex flex-row">
+            <h1 className=" absolute top-14 left-13">sidebar</h1>
+    </div>
+    )
+}
+
+
+export default Sidebar;

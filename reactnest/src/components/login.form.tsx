93e034ba65ec21681bef12667c71b043aa2d4f03
Akshay Kumar <taara0361@gmail.com>
2026-04-23 00:41:55 +0530

    nest frontend

---
+++ reactnest/src/components/login.form.tsx
@@ -0,0 +1,18 @@
+const LoginForm=()=>{
+    return(
+        <div>
+        <form>
+            <div>
+            <label>Email</label>
+            <input />
+            </div>
+            <div>
+            <label>Password</label>
+            <input />
+            </div>
+        </form>
+        </div>
+    )
+}
+
+export default LoginForm;
